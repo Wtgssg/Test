9dd5a2595458f932acf0742e02b83f8a15d7b02b
succeed <succeed>
2026-08-19 19:00:31 +0800

    0.1.3 dev

--- src/main/java/com/success/Main.java
+++ src/main/java/com/success/Main.java
@@ -41,6 +41,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("dev分支");
         System.out.println("0.0.1 dev");
-        System.out.println("error");
+        System.out.println("New Big error");
     }
 }

6b24141fc49bc019d84c5fa077c8d2b786643868
Wtgssg <118713196+Wtgssg@users.noreply.github.com>
2026-08-20 15:56:53 +0800

    新增评价拉完了

--- src/main/java/com/success/Main.java
+++ src/main/java/com/success/Main.java
@@ -39,7 +39,7 @@ import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("master分支");
         System.out.println("0.1.1");
+        System.out.println("我完成了一次评价:拉完了");
     }
 }

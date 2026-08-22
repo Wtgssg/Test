371aadf2ac18779a797f128c4327ee472036f853
succeed <succeed>
2026-08-22 18:18:25 +0800

    commit 1fix

--- src/main/java/com/success/Main.java
+++ src/main/java/com/success/Main.java
@@ -51,6 +51,6 @@ public class Main {
         System.out.println("用户J完成了一次评价:?..");
         System.out.println(1);
         System.out.println(2);
-        System.out.println(3);
+        System.out.println("修改commit1 ");
     }
 }
